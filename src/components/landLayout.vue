fe4ed85c5c500e042cf90aa3b47dde86c31b085d
Rigin <riginlining2@gmail.com>
2019-11-19 00:35:39 +0700

    Tugas

---
+++ src/components/landLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+   
+
+   
+
+    <VContent>
+      <router-view />
+    </VContent>
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,59 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-    const DashboardLayout = () => import(/* webpackChunkName: "dashboard" */ '../components/dashboardLayout.vue')
-    function loadView(view) {
-        return () => import(/* webpackChunkName: "view-[request]" */ `../components/dashboardContents/${view}.vue`)
-    }
-    const routes = [
-        {
-            path: '/',
-            component: DashboardLayout,
-            children: [
-                {
-                    name: 'UserController',
-                    path: '/',
-                    component: loadView('userController')
-                },
-                {
-                    path: '/sparepart',
-                    component: DashboardLayout,
-                    children: [
-                        {
-                            name: 'SparepartCOntroller',
-                            path: '',
-                            component: loadView('sparepartController')
-                        }
-                    ]
-                },
-            ]
-        },
-        
-     ]
-    Vue.use(Router)
-    const router = new Router({mode: 'history', routes: routes})
-    export default router
+
+const DashboardLayout = () => import(/* webpackChunkName: "dashboard" */ '../components/dashboardLayout.vue')
+const LandLayout = () => import(/* webpackChunkName: "dashboard" */ '../components/landLayout.vue')
+
+function loadView(view) {
+    return () => import(/* webpackChunkName: "view-[request]" */ `../components/dashboardContents/${view}.vue`)
+}
+function loadHome(view) {
+    return () => import(`../components/contentLanding/${view}.vue`)
+}
+
+const routes = [
+    {
+        path: '/',
+        component: LandLayout,
+        children: [
+            {
+                name: 'LandingHome',
+                path: '',
+                component: loadHome('landingHome')
+            }
+        ]
+    },
+    {
+        path: '/dashboard',
+        component: DashboardLayout,
+        children: [
+            {
+
+                name: 'login',
+                path: '/login',
+                component: loadView('login')
+
+            },
+            {
+
+                name: 'SparepartController',
+                path: '/sparepartController',
+                component: loadView('sparepartController')
+
+            },
+            {
+                name: 'UserController',
+                path: '/userController',
+                component: loadView('userController')
+            },
+
+        ]
+    },
+
+
+]
+Vue.use(Router)
+
+const router = new Router({ mode: 'history', routes: routes })
+
+export default router
